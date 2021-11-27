c57b46a5e00ba37ea7ae85152feec8cb06041832
Oleg Reutskiy <o.reutskiy@corp.mail.ru>
2021-11-27 22:04:15 +0300

    add validation

--- TraveLite/TraveLite/AuthRegScreen/AuthRegScreenController.swift
+++ TraveLite/TraveLite/AuthRegScreen/AuthRegScreenController.swift
@@ -4,7 +4,7 @@ final class AuthRegScreenViewController: UIViewController {
     private let output: AuthRegScreenViewOutput
     
     private var isAuthScreen = true
-
+    
     let textLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
@@ -19,16 +19,48 @@ final class AuthRegScreenViewController: UIViewController {
         let input = InputView()
         input.placeholder = "Введите логин"
         input.translatesAutoresizingMaskIntoConstraints = false
-        
+        input.addTarget(self, action: #selector(checkLogin), for: .editingChanged)
+        input.addTarget(self, action: #selector(checkLogin), for: .editingDidBegin)
+
         return input
+    }()
+    
+    let loginInputSuggestMes: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = UIColor(red: 0.98, green: 0.82, blue: 0.76, alpha: 1.00)
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.text = "Логин должен быть длиннее 4 символов"
+        label.isHidden = true
+        
+        return label
     }()
     
     let emailInput: InputView = {
         let input = InputView()
         input.placeholder = "Введите e-mail"
         input.translatesAutoresizingMaskIntoConstraints = false
+        input.addTarget(self, action: #selector(checkValidEmail), for: .editingChanged)
+        input.addTarget(self, action: #selector(checkValidEmail), for: .editingDidBegin)
         
         return input
+    }()
+    
+    let emailInputSuggestMes: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = UIColor(red: 0.98, green: 0.82, blue: 0.76, alpha: 1.00)
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.text = "Введите корректный email"
+        label.isHidden = true
+
+        return label
     }()
     
     let passwordInput: InputView = {
@@ -37,7 +69,24 @@ final class AuthRegScreenViewController: UIViewController {
         input.isSecureTextEntry = true
         input.translatesAutoresizingMaskIntoConstraints = false
         
+        input.addTarget(self, action: #selector(checkPassword), for: .editingChanged)
+        input.addTarget(self, action: #selector(checkPassword), for: .editingDidBegin)
+        
         return input
+    }()
+    
+    let passwordInputSuggestMes: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = UIColor(red: 0.98, green: 0.82, blue: 0.76, alpha: 1.00)
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.text = "Пароль должен быть длиннее 8 символов"
+        label.isHidden = true
+
+        return label
     }()
     
     let confirmPasswordInput: InputView = {
@@ -46,7 +95,24 @@ final class AuthRegScreenViewController: UIViewController {
         input.isSecureTextEntry = true
         input.translatesAutoresizingMaskIntoConstraints = false
         
+        input.addTarget(self, action: #selector(confirmPassword), for: .editingChanged)
+        input.addTarget(self, action: #selector(confirmPassword), for: .editingDidBegin)
+        
         return input
+    }()
+    
+    let confirmInputSuggestMes: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = UIColor(red: 0.98, green: 0.82, blue: 0.76, alpha: 1.00)
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.lineBreakMode = .byWordWrapping
+        label.numberOfLines = 0
+        label.text = "Пароли должны совпадать"
+        label.isHidden = true
+
+        return label
     }()
     
     let submitButton: SubmitButtonView = {
@@ -84,6 +150,7 @@ final class AuthRegScreenViewController: UIViewController {
         view.backgroundColor = .white
         
         modalPresentationStyle = .fullScreen
+        
     }
     
     @available(*, unavailable)
@@ -232,33 +299,65 @@ final class AuthRegScreenViewController: UIViewController {
         textLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 7/8).isActive = true
         
         contentView.addSubview(loginInput)
+        loginInput.text = ""
+        loginInput.layer.borderColor = UIColor.gray.cgColor
         loginInput.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         loginInput.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 15).isActive = true
         loginInput.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 7/8).isActive = true
         loginInput.heightAnchor.constraint(equalToConstant: 55).isActive = true
         
+        contentView.addSubview(loginInputSuggestMes)
+        loginInputSuggestMes.isHidden = true
+        loginInputSuggestMes.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        loginInputSuggestMes.topAnchor.constraint(equalTo: loginInput.bottomAnchor, constant: 2).isActive = true
+        loginInputSuggestMes.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 7/8).isActive = true
+        
         contentView.addSubview(emailInput)
+        emailInput.text = ""
+        emailInput.layer.borderColor = UIColor.gray.cgColor
         emailInput.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        emailInput.topAnchor.constraint(equalTo: loginInput.bottomAnchor, constant: 15).isActive = true
+        emailInput.topAnchor.constraint(equalTo: loginInputSuggestMes.bottomAnchor, constant: 7).isActive = true
         emailInput.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 7/8).isActive = true
         emailInput.heightAnchor.constraint(equalToConstant: 55).isActive = true
         
+        contentView.addSubview(emailInputSuggestMes)
+        emailInputSuggestMes.isHidden = true
+        emailInputSuggestMes.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        emailInputSuggestMes.topAnchor.constraint(equalTo: emailInput.bottomAnchor, constant: 2).isActive = true
+        emailInputSuggestMes.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 7/8).isActive = true
+        
         contentView.addSubview(passwordInput)
+        passwordInput.text = ""
+        passwordInput.layer.borderColor = UIColor.gray.cgColor
         passwordInput.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        passwordInput.topAnchor.constraint(equalTo: emailInput.bottomAnchor, constant: 15).isActive = true
+        passwordInput.topAnchor.constraint(equalTo: emailInputSuggestMes.bottomAnchor, constant: 7).isActive = true
         passwordInput.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 7/8).isActive = true
         passwordInput.heightAnchor.constraint(equalToConstant: 55).isActive = true
         
+        contentView.addSubview(passwordInputSuggestMes)
+        passwordInputSuggestMes.isHidden = true
+        passwordInputSuggestMes.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        passwordInputSuggestMes.topAnchor.constraint(equalTo: passwordInput.bottomAnchor, constant: 2).isActive = true
+        passwordInputSuggestMes.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 7/8).isActive = true
+        
         contentView.addSubview(confirmPasswordInput)
+        confirmPasswordInput.text = ""
+        confirmPasswordInput.layer.borderColor = UIColor.gray.cgColor
         confirmPasswordInput.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        confirmPasswordInput.topAnchor.constraint(equalTo: passwordInput.bottomAnchor, constant: 15).isActive = true
+        confirmPasswordInput.topAnchor.constraint(equalTo: passwordInputSuggestMes.bottomAnchor, constant: 7).isActive = true
         confirmPasswordInput.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 7/8).isActive = true
         confirmPasswordInput.heightAnchor.constraint(equalToConstant: 55).isActive = true
+        
+        contentView.addSubview(confirmInputSuggestMes)
+        confirmInputSuggestMes.isHidden = true
+        confirmInputSuggestMes.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        confirmInputSuggestMes.topAnchor.constraint(equalTo: confirmPasswordInput.bottomAnchor, constant: 2).isActive = true
+        confirmInputSuggestMes.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 7/8).isActive = true
         
         contentView.addSubview(submitButton)
         submitButton.setTitle("Создать аккаунт", for: .normal)
         submitButton.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        submitButton.topAnchor.constraint(equalTo: confirmPasswordInput.bottomAnchor, constant: 15).isActive = true
+        submitButton.topAnchor.constraint(equalTo: confirmInputSuggestMes.bottomAnchor, constant: 15).isActive = true
         submitButton.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 7/8).isActive = true
         submitButton.heightAnchor.constraint(equalToConstant: 55).isActive = true
         
@@ -286,8 +385,9 @@ final class AuthRegScreenViewController: UIViewController {
         
         textLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 7/8).isActive = true
         
-        
         contentView.addSubview(emailInput)
+        emailInput.text = ""
+        emailInput.layer.borderColor = UIColor.gray.cgColor
         emailInput.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         emailInput.topAnchor.constraint(equalTo: textLabel.bottomAnchor, constant: 15).isActive = true
         emailInput.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 7/8).isActive = true
@@ -295,6 +395,8 @@ final class AuthRegScreenViewController: UIViewController {
         
         
         contentView.addSubview(passwordInput)
+        passwordInput.text = ""
+        passwordInput.layer.borderColor = UIColor.gray.cgColor
         passwordInput.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         passwordInput.topAnchor.constraint(equalTo: emailInput.bottomAnchor, constant: 15).isActive = true
         passwordInput.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 7/8).isActive = true
@@ -316,6 +418,71 @@ final class AuthRegScreenViewController: UIViewController {
         regAuthButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
         
         self.view.layoutIfNeeded()
+    }
+    
+    @objc
+    func checkLogin(_ sender: UITextField) {
+        if isAuthScreen {
+            return
+        }
+        if sender.text?.count ?? 0 < 4 {
+            sender.layer.borderColor = UIColor.red.cgColor
+            loginInputSuggestMes.isHidden = false
+        } else {
+            sender.layer.borderColor = UIColor.green.cgColor
+            loginInputSuggestMes.isHidden = true
+        }
+    }
+    
+    @objc
+    func checkPassword(_ sender: UITextField) {
+        if isAuthScreen {
+            return
+        }
+        if sender.text?.count ?? 0 > 8 {
+            sender.layer.borderColor = UIColor.green.cgColor
+            passwordInputSuggestMes.isHidden = true
+        } else {
+            sender.layer.borderColor = UIColor.red.cgColor
+            passwordInputSuggestMes.isHidden = false
+        }
+    }
+    
+    
+    @objc
+    func checkValidEmail(_ sender: UITextField) {
+        if isAuthScreen {
+            return
+        }
+        if isValidEmail(sender.text ?? "") {
+            sender.layer.borderColor = UIColor.green.cgColor
+            emailInputSuggestMes.isHidden = true
+        } else {
+            sender.layer.borderColor = UIColor.red.cgColor
+            emailInputSuggestMes.isHidden = false
+        }
+    }
+    
+    @objc
+    func confirmPassword(_ sender: UITextField) {
+        if isAuthScreen {
+            sender.layer.borderColor = UIColor.gray.cgColor
+            return
+        }
+        if sender.text == passwordInput.text && sender.text != "" {
+            sender.layer.borderColor = UIColor.green.cgColor
+            confirmInputSuggestMes.isHidden = true
+        } else {
+            sender.layer.borderColor = UIColor.red.cgColor
+            confirmInputSuggestMes.isHidden = false
+        }
+    }
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
     }
     
 }
